b6c1338519ff454b37a0947d5d28e9f1e10e5cd0
AngelosTheodorakis <ajotheone@gmail.com>
2020-04-20 21:18:59 +0300

    gr

--- My_encoding.R
+++ My_encoding.R
@@ -6,31 +6,32 @@ summary(data)
 
 Missing_values<-data.frame(sort(sapply(data,function(x) sum(is.na(x))),decreasing = TRUE))
 
+
 #Q7
 #In variable Q7 
-test<-data.frame(cbind(data$Q6,data$Q7))
-#prepei na simpiptoun ta Na's tis erotisis 6 me ta na's ths 7     
-which((test[,1]<=15) & is.na(test[,2]==TRUE))
+#test<-data.frame(cbind(data$Q6,data$Q7))
+ #prepei na simpiptoun ta Na's tis erotisis 6 me ta na's ths 7     
+#which((test[,1]<=15) & is.na(test[,2]==TRUE))
 #Ara vriskoume missing values stis paratiriseis 121 kai 463 ta opoia
 #aforoun atoma pou ergazontai alla den apantisan.
-data[c(121,463),]
+#data[c(121,463),]
 #Twra tha vroume poia atoma den exoun apantisei stin erotisi 6 
 # kai an afta simpiptoun me afta tis erotisis 7
-which(is.na(data$Q6) & is.na(data$Q7))
-identical(which(is.na(data$Q6)),which(is.na(data$Q6) & is.na(data$Q7)))
+#which(is.na(data$Q6) & is.na(data$Q7))
+#identical(which(is.na(data$Q6)),which(is.na(data$Q6) & is.na(data$Q7)))
 #epomenws ontws ta missing values ths Q6 antistoixoun stis idies paratiriseis
 #tis q7. autes einai  68  72 169 184 243 338 351 360 391 430 445 453
 
 #Ara sinolika ta missing values ths Q7 einai
 # 68 72 121 169 184 243 338 351 360 391 430 445 453 kai 463
-length(which(is.na(data$Q6) & is.na(data$Q7))) + 
-  length( which((test[,1]<=15) & is.na(test[,2]==TRUE)))
+#length(which(is.na(data$Q6) & is.na(data$Q7))) + 
+ #length( which((test[,1]<=15) & is.na(test[,2]==TRUE)))
 #14 missing values gia tin Q7
 
 
 #Q13
-data[which(is.na(data$Q13)),]
-which(is.na(data$Q13)) #Paratiriseis me missing values gia tin Q13
+# data[which(is.na(data$Q13)),]
+# which(is.na(data$Q13)) #Paratiriseis me missing values gia tin Q13
 
 #Q10_1
 data[which(is.na(data$Q10_1)),]
@@ -125,8 +126,26 @@ data[which(is.na(data$Q4)),]
 library(dplyr)
 data$Q3<-as.factor(data$Q3)
 data$Q13<-as.numeric(data$Q13)
-data %>% 
-  count(Q3,Q13)
+data %>% group_by(Q3)%>%group_by(Gradyear)%>%
+  summarise(mean(na.omit(Q5_SQ001)))
+
+test<-data.frame(data %>% group_by(Q3,Gradyear)%>%
+  summarise(mean(na.omit(Q5_SQ001))))
+
+plot(x=test[1:4,"Gradyear"],y=test[1:4,"mean.na.omit.Q5_SQ001.."],ylim = c(0,10),type = 'b',pch=as.character(as.numeric(test[1:4,"mean.na.omit.Q5_SQ001.."])))
+#Plot που απεικονίζει τον μ.ο της βαθμολόγησης του ΠΜΣ ανα χρονική περίοδο
+plot(x=test[1:4,"Gradyear"],y=test[1:4,"mean.na.omit.Q5_SQ001.."],ylim = c(0,10),type = 'b')
+text(test[1:4,"Gradyear"],test[1:4,"mean.na.omit.Q5_SQ001.."],label=round(test[1:4,"mean.na.omit.Q5_SQ001.."],2),col='blue',cex=.8,pos = 3)
+
+plot(x=test[5:8,"Gradyear"],y=test[5:8,"mean.na.omit.Q5_SQ001.."],ylim = c(0,10),type = 'b')
+text(test[5:8,"Gradyear"],test[5:8,"mean.na.omit.Q5_SQ001.."],label=round(test[5:8,"mean.na.omit.Q5_SQ001.."],2),col='blue',cex=.8,pos = 3)
+
+plot(x=test[9:12,"Gradyear"],y=test[9:12,"mean.na.omit.Q5_SQ001.."],ylim = c(0,10),type = 'b')
+text(test[9:12,"Gradyear"],test[9:12,"mean.na.omit.Q5_SQ001.."],label=round(test[9:12,"mean.na.omit.Q5_SQ001.."],2),col='blue',cex=.8,pos = 3)
+
+plot(x=test[13:16,"Gradyear"],y=test[13:16,"mean.na.omit.Q5_SQ001.."],ylim = c(0,10),type = 'b')
+text(test[13:16,"Gradyear"],test[13:16,"mean.na.omit.Q5_SQ001.."],label=round(test[13:16,"mean.na.omit.Q5_SQ001.."],2),col='blue',cex=.8,pos = 3)
+
 
 
 #OR
@@ -140,7 +159,7 @@ source("http://peterhaschke.com/Code/multiplot.R")
 
 
 
-ggplot(data, aes(x = factor(Gradyear))) +  
+p3<-ggplot(data, aes(x = factor(Gradyear))) +  
   geom_bar(aes(y = (..count..)/sum(..count..), fill = factor(Gradyear))) + 
   geom_text(aes( label = scales::percent((..count..)/sum(..count..)),
                  y=(..count..)/sum(..count..) ), stat= "count", vjust = -.3) +
@@ -161,7 +180,7 @@ p1<-ggplot(data, aes(x = factor(Q1))) +
   theme(plot.title = element_text(hjust = 0.5))+
   scale_x_discrete(labels=c('Γυναίκες','’νδρες'))+
   scale_fill_discrete(name = "Φύλο", 
-                      labels=c("Γυναίκες", "’νδρες"))
+                       labels=c("Γυναίκες", "’νδρες"))
 
 
 p2<-ggplot(data, aes(x = factor(Age))) +  
@@ -196,11 +215,71 @@ data.frame(attr(data$Emplmnt,"value.labels"))
 #As kanoume ena grafima pou afora tin aksiologisi ana filo
 
 
-data$Q13
+#data$Q13
+#install.packages('egg')
+library(egg)
 
 ggplot(data, aes(x = factor(Q13),fill=factor(Q13))) +  
   geom_bar(width=0.5) + 
-  labs(fill='Θα συστήνατε?')
+  labs(fill='Θα συστήνατε 
+το μεταπτυχιακό?')
+
+ggplot(data, aes(x = factor(Q13))) +  
+  geom_bar(aes(y = (..count..)/sum(..count..), fill = factor(Q13))) + 
+  geom_text(aes( label = scales::percent((..count..)/sum(..count..)),
+                 y=(..count..)/sum(..count..) ), stat= "count", vjust = -.3) +
+  scale_y_continuous(labels = percent) + 
+  labs(title='Θα συστήνατε το μεταπτυχιακό?', x='Απάντηση',y='Πλήθος',
+       fill='Θα συστήνατε 
+το μεταπτυχιακό?')+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_discrete(labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))+
+scale_fill_discrete(labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))
+
+
+data$Q3Factor <- factor(data$Q3, labels = c("Πολιτική Επιστήμη και Κοινωνιολογία", "Διεθνείς και Ευρωπαϊκές σπουδές", "Κράτος και Δημόσια Πολιτική", "Σπουδές Νοτ/ης Ευρώπης"))
+
+
+ggplot(data, aes(x = factor(Q13),fill=factor(Q13))) +  
+  geom_bar(width=0.5) + 
+  labs(title='Θα συστήνατε το μεταπτυχιακό;
+(Διαχωρισμός ανα τμήμα)',
+       x='Απάντηση',y='Πλήθος')+
+  facet_wrap(~factor(Q3Factor))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_discrete(labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))+
+  scale_fill_discrete(name='Απάντηση',labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))
+
+ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+  geom_bar(width=0.5) + 
+  labs(title='Θα συστήνατε το μεταπτυχιακό;
+(Διαχωρισμός ανα τμήμα και φύλο)',
+       x='Απάντηση',y='Πλήθος')+
+  facet_wrap(~factor(Q3Factor))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_discrete(labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))+
+  scale_fill_discrete(name='Φύλο',labels=c('Γυναίκες','’νδρες'))
+
+
+
+
+
+#ME POSOSTO stat(density)
+# library(dplyr)
+# dfl <- data %>% 
+#   group_by(Q13,Q3Factor) %>% 
+#   summarise(n=n()) %>% 
+#   group_by(factor(Q13)) %>% 
+#   mutate(perc=100*n/sum(n))
+# 
+# ggplot(dfl, aes(x = factor(Q13), y=perc, fill= factor(Q13))) +
+#   geom_bar(stat="identity") +
+#   ylab("percent") + 
+#   facet_wrap(~ Q3Factor)
+
+
+
+#ggarrange(p1,p2,ncol = 1)
 
 plot(as.factor(data$Q13))
 table(as.factor(data$Q13))
@@ -216,52 +295,77 @@ ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +
   facet_wrap(~factor(Q5_SQ001))+
   labs(fill='Gender')
 
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
-  geom_bar(width=0.5) + 
-  facet_wrap(~factor(Age))+
-  labs(fill='Gender')
 
-#LABELS AGE#
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+
+ggplot(data, aes(x = factor(Q13),fill=factor(Q13))) +  
   geom_bar(width=0.5) + 
-  labs(title='Γνώμη για σύσταση ΠΜΣ ανά Ηλικίακή ομάδα', x='Γνώμη',y='Πλήθος')+
-  facet_wrap(~factor(Age))+
+  labs(title='Πρόταση ΠΜΣ
+(Διαχωρισμός ανα Βαθμό Ικανοποίησης από τις σπουδές)',
+       x='Βαθμολογία',y='Πλήθος')+
+  facet_wrap(~factor(Q5_SQ001))+
   theme(plot.title = element_text(hjust = 0.5))+
-  scale_x_discrete(labels = c("Σίγουρα ναι", "Μάλλον ναι", "Ούτε ναι ούτε οχι", "Μάλλον όχι", "Σίγουρα όχι"))+
-  scale_fill_discrete(name = "Φύλο", 
-                      labels=c("Γυναίκες", "’νδρες"))
+  scale_fill_discrete(name="Θα συστήνατε 
+το ΠΜΣ;",
+                      labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))
 
 
 
 
 
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
-  geom_bar(width=0.5) + 
-  facet_wrap(~factor(Age2cats))+
+
+
+
+
+
+
+
+
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    facet_wrap(~factor(Age))+
+  labs(fill='Gender')
+  
+  #LABELS AGE#
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    labs(title='Γνώμη για σύσταση ΠΜΣ ανά Ηλικίακή ομάδα', x='Γνώμη',y='Πλήθος')+
+    facet_wrap(~factor(Age))+
+    theme(plot.title = element_text(hjust = 0.5))+
+    scale_x_discrete(labels = c("Σίγουρα ναι", "Μάλλον ναι", "Ούτε ναι ούτε οχι", "Μάλλον όχι", "Σίγουρα όχι"))+
+    scale_fill_discrete(name = "Φύλο", 
+                        labels=c("Γυναίκες", "’νδρες"))
+  
+  
+  
+  
+  
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    facet_wrap(~factor(Age2cats))+
   labs(fill='Gender')
 
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
-  geom_bar(width=0.5) + 
-  facet_wrap(~factor(Q7))+
-  labs(fill='Gender')
-
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
-  geom_bar(width=0.5) + 
-  facet_wrap(~factor(language))+
-  labs(fill='Gender')
-
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
-  geom_bar(width=0.5) + 
-  facet_wrap(~ factor(Q3))+
-  labs(fill='Gender')
-
-
-ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
-  geom_bar(width=0.5) + 
-  facet_wrap(~ factor(Q8))+
-  labs(fill='Gender')
-
-
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    facet_wrap(~factor(Q7))+
+    labs(fill='Gender')
+  
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    facet_wrap(~factor(language))+
+    labs(fill='Gender')
+  
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    facet_wrap(~ factor(Q3))+
+    labs(fill='Gender')
+ 
+  
+  ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
+    geom_bar(width=0.5) + 
+    facet_wrap(~ factor(Q8))+
+    labs(fill='Gender')
+  
+  
 ggplot(data, aes(x = factor(Q13),fill=factor(Age))) +  
   geom_bar(width=0.5) + 
   labs(fill='Age group')
@@ -291,8 +395,8 @@ ggplot(data, aes(x = factor(Q13),fill = factor(Q8))) +
        fill='Συνάφεια με εργασία')+
   theme(plot.title = element_text(hjust = 0.5))+
   scale_x_discrete(labels=c('Σίγουρα ναι','Μάλλον ναι','Ούτε ναι ούτε όχι','Μάλλον όχι','Σίγουρα όχι'))+
-  scale_fill_manual(values=c("darkred","red","blue","green4","darkgreen","yellow"),
-                    labels = c("Πολύ ", "Αρκετά ", "Όχι και τόσο ", "Καθόλου ", "Δεν έχω εργαστεί",'NA'))
+scale_fill_manual(values=c("darkred","red","blue","green4","darkgreen","yellow"),
+  labels = c("Πολύ ", "Αρκετά ", "Όχι και τόσο ", "Καθόλου ", "Δεν έχω εργαστεί",'NA'))
 
 
 ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +  
@@ -301,7 +405,10 @@ ggplot(data, aes(x = factor(Q13),fill=factor(Q1))) +
   labs(fill='Gender')
 
 
-plot(as.factor(data$age2017))
+plot(as.factor(data$age2017)) #further investigate for normal distribution
+median(data$age2017)
+mean(data$age2017)
+
 
 
 data$work <- rep('’νεργοι',nrow(data))
@@ -400,7 +507,7 @@ ggplot(data, aes(x = factor(Q5_SQ001))) +
   labs(title='Βαθμός Ικανοποίησης', x='',y='Πλήθος',
        fill='')+
   theme(plot.title = element_text(hjust = 0.5))
-
+ 
 ggplot(data, aes(x = factor(Q5_SQ001))) +  
   geom_bar(aes(y = (..count..)/sum(..count..), fill = factor(Age))) + 
   geom_text(aes( label = scales::percent((..count..)/sum(..count..)),
@@ -428,9 +535,9 @@ ggplot(data, aes(x = factor(Q5_SQ001))) +
   labs(title='Βαθμός Ικανοποίησης', x='',y='Πλήθος',
        fill='Φύλο')+
   theme(plot.title = element_text(hjust = 0.5))+
-  facet_wrap(~factor(Q3))
+ facet_wrap(~factor(Q3))
 
-
+ 
 ggplot(data, aes(x = factor(Q5_SQ001))) +  
   geom_bar(aes(y = (..count..), fill = factor(Gradyear))) +
   geom_text(stat='count', aes(label=..count..), vjust=-.3) +
